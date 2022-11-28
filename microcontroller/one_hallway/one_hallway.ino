36ab9df75353788b9845aa4a742eb4b234c94c1c
Jonathan Raditya <jonathanraditya@live.com>
2022-11-28 20:20:40 +0700

    Input/output py server and db for storing data every seconds. Microcontroller code for input/output tx/rx from server.

---
+++ microcontroller/one_hallway/one_hallway.ino
@@ -0,0 +1,233 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "Kampung Bali_EXT";
+const char* password = "(//////)";
+
+//“hwid”
+//  “two_corner”
+//    “front_facing” -> 15 IN
+//    “side_facing” -> 2 IN
+//    “room_entry” -> 4 IN
+//    “reset_trigger” -> 16 OUT
+//  “two_entry”
+//    “left_sensor” 15 IN
+//    “right_sensor” 2 IN
+//    “reset_trigger” -> 4 OUT
+//  “one_perimeter”
+//    “carport_perimeter” 15 IN
+//    “large_gate” 2 IN
+//    “small_gate” 4 IN
+//    “garden_fence” 16 IN
+//    “garden_outer_perimeter” 17 IN
+//    “reset_trigger” -> 5 OUT
+//  “one_main”
+//    “parlor_window” 15 IN
+//    “main_door_upper” 2 IN
+//    “main_door_lower” 4 IN
+//    “reset_trigger” -> 16 OUT
+//  “one_hallway”
+//    “hallway_door” 15 IN
+//    “hallway_window” 2 IN
+//    “kitchen_lamp_switch” 4 OUT
+//    “outdoor_lamp_switch” 16 OUT
+//    “parlor_lamp_switch” 17 OUT
+//    “reset_trigger” -> 5 OUT
+
+// THINGS NEED TO BE CHANGED:
+// hwid (~43 & ~45) 
+// pinMode setup according to pin rule (~88)
+// ditialRead (~109)
+// store input cumulative (~140)
+// payload substring in output command (~164)
+// relay update (~168)
+
+// one_hallway SETUP (IN, IN, OUT, OUT, OUT, OUT)
+
+String hwid = "one_hallway";
+String key = "431f8a00bb579d4691b19efe332e21d8b61ff25e8a1698d81bf95a756660f3ee";
+String input_url = "http://192.168.0.5:5000/input?key=" + key + "&hwid=" + hwid + "&payload=";
+String output_url = "http://192.168.0.5:5000/output?key=" + key + "&hwid=" + hwid;
+
+const int pin1 = 15;
+const int pin2 = 2;
+const int pin3 = 4;
+const int pin4 = 16;
+const int pin5 = 17;
+const int pin6 = 5;
+
+
+// Store sensor status
+int pin1_status = 0;
+int pin1_cumulative = 0;
+String pin1_output_status = "";
+int pin2_status = 0;
+int pin2_cumulative = 0;
+String pin2_output_status = "";
+int pin3_status = 0;
+int pin3_cumulative = 0;
+String pin3_output_status = "";
+int pin4_status = 0;
+int pin4_cumulative = 0;
+String pin4_output_status = "";
+int pin5_status = 0;
+int pin5_cumulative = 0;
+String pin5_output_status = "";
+int pin6_status = 0;
+int pin6_cumulative = 0;
+String pin6_output_status = "";
+int send_data_every = 1000; // ms
+int delay_set = 5; // ms
+int counter = 0;
+int counter_reference = send_data_every / delay_set;
+int wifi_connection = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  // initialize pinout
+  pinMode(pin1, INPUT);
+  pinMode(pin2, INPUT);
+  pinMode(pin3, OUTPUT);
+  pinMode(pin4, OUTPUT);
+  pinMode(pin5, OUTPUT);
+  pinMode(pin6, OUTPUT);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+  wifi_connection = 1;
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pin1_status = digitalRead(pin1);
+  pin1_cumulative = pin1_cumulative + pin1_status;
+  pin2_status = digitalRead(pin2);
+  pin2_cumulative = pin2_cumulative + pin2_status;
+//  pin3_status = digitalRead(pin3);
+//  pin3_cumulative = pin3_cumulative + pin3_status;
+  
+  counter = counter + 1;
+  if (counter == counter_reference){
+    // Post data to server
+    if(WiFi.status() == WL_CONNECTED){
+       wifi_connection = 1;
+      }
+    else {
+      // Reconnect to WiFi
+      wifi_connection = 0;
+      WiFi.begin(ssid, password);
+      Serial.println("Reconnecting");
+      while(WiFi.status() != WL_CONNECTED) {
+          delay(500);
+          Serial.print(".");
+        }
+      wifi_connection = 1;
+      Serial.println("");
+      Serial.print("Reconnected to WiFi network with IP Address: ");
+      Serial.println(WiFi.localIP());
+      }
+    HTTPClient http;
+
+    // Store Input
+    String input_url_w_data = input_url + pin1_cumulative + "z" + pin2_cumulative;
+    Serial.println(input_url_w_data);
+    http.begin(input_url_w_data.c_str());
+    int input_http_response_code = http.GET();
+
+    if (input_http_response_code > 0) {
+      Serial.print("Input HTTP Response code: ");
+      Serial.println(input_http_response_code);
+      }
+    else {
+      Serial.print("Input error code: ");
+      Serial.println(input_http_response_code);
+      }
+    http.end();
+
+    // Get output params
+    http.begin(output_url.c_str());
+    int output_http_response_code = http.GET();
+
+    if (output_http_response_code > 0) {
+      Serial.print("Output HTTP Response code: ");
+      Serial.println(output_http_response_code);
+      String payload = http.getString();
+      payload = String(payload);
+      Serial.println(payload);
+      pin3_output_status = payload.substring(0,1);
+      pin4_output_status = payload.substring(2,3);
+      pin5_output_status = payload.substring(4,5);
+      pin6_output_status = payload.substring(6,7);
+
+      // Pin 3 Relay update
+      if(pin3_output_status == "0"){
+        Serial.println("Pin 3 Low state (OFF)");
+        pinMode(pin3, INPUT);
+        }
+      else {
+        Serial.println("Pin 3 High state (ON)");
+        pinMode(pin3, OUTPUT);
+        }
+
+      // Pin 4 Relay update
+      if(pin4_output_status == "0"){
+        Serial.println("Pin 4 Low state (OFF)");
+        pinMode(pin4, INPUT);
+        }
+      else {
+        Serial.println("Pin 4 High state (ON)");
+        pinMode(pin4, OUTPUT);
+        }
+
+      // Pin 5 Relay update
+      if(pin5_output_status == "0"){
+        Serial.println("Pin 5 Low state (OFF)");
+        pinMode(pin5, INPUT);
+        }
+      else {
+        Serial.println("Pin 5 High state (ON)");
+        pinMode(pin5, OUTPUT);
+        }
+
+      // Pin 6 Relay update
+      if(pin6_output_status == "0"){
+        Serial.println("Pin 6 Low state (OFF)");
+        pinMode(pin6, INPUT);
+        }
+      else {
+        Serial.println("Pin 6 High state (ON)");
+        pinMode(pin6, OUTPUT);
+        }
+
+        
+      }
+    else {
+      Serial.print("Output error code: ");
+      Serial.println(output_http_response_code);
+      }
+    http.end();
+
+
+    
+
+    // Reset counter
+    counter = 0;
+    pin1_cumulative = 0;
+    pin2_cumulative = 0;
+    pin3_cumulative = 0;
+    pin4_cumulative = 0;
+    pin5_cumulative = 0;
+    pin6_cumulative = 0;
+    }
+  delay(delay_set);
+
+}
---
+++ server/main.py
@@ -0,0 +1,184 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+import sqlite3
+import os
+from datetime import datetime
+import pytz
+import hashlib
+import random
+import time
+
+class GlobalOperations:   
+    def datetime_now(self):
+        tz = pytz.timezone('Asia/Jakarta')
+        return datetime.now(tz)
+    
+    def gethash(self, string):
+        '''sha256'''
+        # sanity check
+        if not isinstance(string, str):
+            string = str(string)            
+        return hashlib.sha256(bytes(string, 'utf-8')).hexdigest()
+
+class Sqlite(GlobalOperations):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.conn = None
+        self.table = None
+        
+    def set_table(self, table):
+        self.table = table
+        
+    def create_connection(self, db_path):
+        try:
+            self.conn = sqlite3.connect(db_path)
+        except Error as e:
+            print(f'Failed to create connection. {e}')
+    
+    def create_table(self, col_dtypes):
+        sql = f'''create table if not exists {self.table} ({col_dtypes})'''
+        cur = self.conn.cursor()
+        cur.execute(sql)
+    
+    def insert_value(self, cols, values):            
+        if isinstance(self.conn, sqlite3.Connection) & isinstance(self.table, str):
+            if all(isinstance(i, list) for i in [cols, values]):
+                cols_all = ['datetime']
+                data_placeholders = ['?']
+                values_all = [f"'{str(self.datetime_now().timestamp())}'"]
+                values_all = [str(self.datetime_now().timestamp())]
+                col_dtypes_bind = ['datetime float']
+                
+                for i, col in enumerate(cols):
+                    cols_all.append(col)
+                    data_placeholders.append('?')
+                    # values_all.append(f"'{values[i]}'")
+                    values_all.append(values[i])
+                    col_dtypes_bind.append(f'{col} int')
+                      
+                cols_insert = ','.join(cols_all)
+                data_placeholder_insert = ','.join(data_placeholders)
+                values_insert = ','.join(values_all)
+                print(values_insert)
+                col_dtypes_bind_insert = ','.join(col_dtypes_bind)
+                
+                # Create table if not exists
+                self.create_table(col_dtypes_bind_insert)
+                
+                sql = f'''insert into {self.table} ({cols_insert}) values ({data_placeholder_insert})'''
+                print(sql)
+                cur = self.conn.cursor()
+                cur.execute(sql, values_all)
+                self.conn.commit()
+            else:
+                print('Makesure that colnames is in python list format `[...,...,...]`')
+        else:
+            print('Please initialize connection and table name first.')
+    
+class VibrateProcessor(Sqlite):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.root_dir = os.getcwd()
+        self.db = 'records.db'
+        
+        self.db_path = os.path.join(self.root_dir, self.db)
+
+        self.in_sw = {
+            'two_corner':['front_facing','side_facing','room_entry'],
+            'two_entry':['left_sensor','right_sensor'],
+            'one_perimeter':['carport_perimeter','large_gate','small_gate','garden_fence','garden_outer_perimeter'],
+            'one_main':['parlor_window','main_door_upper','main_door_lower'],
+            'one_hallway':['hallway_door','hallway_window','kitchen_lamp_switch','outdoor_lamp_switch','parlor_lamp_switch']
+        }
+        self.out_gb = 0
+        self.out_sw = {
+            'two_corner':{
+                'reset_trigger':0,
+            },
+            'two_entry':{
+                'reset_trigger':0,
+            },
+            'one_perimeter':{
+                'reset_trigger':0,
+            },
+            'one_main':{
+                'reset_trigger':0,
+            },
+            'one_hallway':{
+                'kitchen_lamp_switch':0,
+                'outdoor_lamp_switch':0,
+                'parlor_lamp_switch':0,
+                'reset_trigger':0
+            }
+        }
+        random.seed(1209218402348023984239874891)
+        self.key = self.gethash(random.random())
+        self.key = self.gethash(self.key)
+        
+        self.host = '192.168.0.5'
+        self.port = 5000
+        
+        
+    
+class Vibrateserver(BaseHTTPRequestHandler, VibrateProcessor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)        
+        
+    def do_GET(self):
+        qs = parse_qs(urlparse(self.path).query)
+        path = urlparse(self.path).path
+        vp = VibrateProcessor()
+        
+        print('zero')
+        if 'key' in qs.keys() and 'hwid' in qs.keys():
+            print('a')
+            hwid = qs['hwid'][0]
+            if qs['key'][0] == vp.key:
+                print('b')
+                # Input handler
+                if path == '/input':
+                    print('c')
+                    if hwid in vp.in_sw.keys():
+                        print('d')
+                        payload_split = qs['payload'][0].split('z')
+                        if len(payload_split) == len(vp.in_sw[hwid]):
+                            print('e')
+                            self.send_response(200)
+                            self.send_header('Content-type', 'text/html')
+                            self.end_headers()
+                            self.set_table(hwid)
+                            self.create_connection(vp.db_path)
+                            print(vp.in_sw[hwid])
+                            print(payload_split)
+                            print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+                            print(hwid)
+                            self.insert_value(vp.in_sw[hwid], payload_split)
+
+                # Output handler
+                elif path == '/output':
+                    print('f')
+                    if hwid in vp.out_sw.keys():
+                        print('g')
+                        self.send_response(200)
+                        self.send_header('Content-type', 'text/html')
+                        self.end_headers()
+                        
+                        output_csv = ','.join([str(v) for v in vp.out_sw[hwid].values()])
+                        
+                        self.wfile.write(bytes(output_csv, 'utf-8'))        
+                        
+        print('last')
+            
+            
+if __name__ == '__main__':
+    vp = VibrateProcessor()
+    webServer = HTTPServer((vp.host,vp.port), Vibrateserver)
+    print(f'Server started http://{vp.host}:{vp.port}')
+    
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    
+    webServer.server_close()
+    print('Server stopped.')
